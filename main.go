84312b89b20165e0c1dcf1119a1305df3dc5d777
Samyak Jain <jainsamyak2300@gmail.com>
2021-10-09 02:08:36 +0530

    Get User Details Endpoint Complete

--- main.go
+++ main.go
@@ -3,27 +3,24 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 import . "AppointyTask/models"
 
-var dbInstance *mongo.Database
-var userCollection *mongo.Collection
-var postCollection *mongo.Collection
-var contextInstance *context.Context
-var dbHandler = BaseHandler{Db: dbInstance, Users: userCollection, Posts: postCollection, Ctx: contextInstance}
+var dbHandler = BaseHandler{}
 
-var users []User
-var allUsers = Users{AllUsers: users}
+//var allUsers = Users{}
 
-var posts []Post
-var allPosts = Posts{AllPosts: posts}
+//var allPosts = Posts{}
 
 var ctx context.Context
 
@@ -54,40 +51,61 @@ func main() {
 	dbHandler.Users = dbHandler.Db.Collection("users")
 	dbHandler.Posts = dbHandler.Db.Collection("posts")
 
-	//http.HandleFunc("/getUsers", getUser)
-	http.HandleFunc("/addUsers", addUser)
+	http.HandleFunc("/users", addUser)
+	http.HandleFunc("/users/", getUser)
 
 	http.ListenAndServe(":8080", nil)
 }
 
+func getUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.Header().Add("Content-Type", "application/json")
+		userId, _ := primitive.ObjectIDFromHex(strings.Split(r.URL.String(), "/users/")[1])
+
+		cursor, err := dbHandler.Users.Find(ctx, bson.M{"_id": userId})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		var currentUser []bson.M
+		if err = cursor.All(ctx, &currentUser); err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(currentUser[0])
+
+	} else {
+		w.Write([]byte("Method Not Allowed: " + r.Method))
+	}
+}
+
 func addUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		decoder := json.NewDecoder(r.Body)
 
-	decoder := json.NewDecoder(r.Body)
+		var user User
 
-	var user User
+		err := decoder.Decode(&user)
+		if err != nil {
+			panic(err)
+		}
 
-	err := decoder.Decode(&user)
-	if err != nil {
-		panic(err)
-	}
+		hashedPassword, passwordHashingErr := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
 
-	hashedPassword, passwordHashingErr := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
+		if passwordHashingErr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
-	if passwordHashingErr != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		user.Password = string(hashedPassword)
 
-	user.Password = string(hashedPassword)
+		one, err := dbHandler.Users.InsertOne(ctx, user)
+		if err != nil {
+			return
+		}
 
-	one, err := dbHandler.Users.InsertOne(ctx, user)
-	if err != nil {
-		return
-	}
-
-	log.Println(one)
-	resErr := json.NewEncoder(w).Encode(one)
-	if resErr != nil {
-		return
+		json.NewEncoder(w).Encode(one)
+	} else {
+		w.Write([]byte("Method Not Allowed: " + r.Method))
 	}
 }
