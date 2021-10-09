e6d75a888cb69b9365dac2d8900d7bbe3b49536a
Samyak Jain <jainsamyak2300@gmail.com>
2021-10-09 13:15:12 +0530

    added User and Post Endpoints in api package

---
+++ api/posts.go
@@ -0,0 +1,134 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+import . "AppointyTask/models"
+
+func AddPost(w http.ResponseWriter, r *http.Request, dbHandler BaseHandler) {
+	if r.Method == "POST" {
+
+		var target map[string]interface{}
+		body, _ := ioutil.ReadAll(r.Body)
+		json.Unmarshal(body, &target)
+
+		var post Post
+
+		user_id_interface, ok := target["user_id"]
+
+		if ok {
+			userId, _ := primitive.ObjectIDFromHex(user_id_interface.(string))
+			post.UserId = userId
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("User Id Not Provided"))
+			return
+		}
+
+		post.PostedTimestamp = time.Now()
+
+		imageUrl, ok := target["image_url"]
+
+		if ok {
+			var imageUrlString = imageUrl.(string)
+			parsedUrl, err := url.Parse(imageUrlString)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			post.ImageUrl = *parsedUrl
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Image URL Not Provided"))
+			return
+		}
+
+		caption, ok := target["caption"]
+
+		if ok {
+			post.Caption = caption.(string)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Caption Not Found"))
+			return
+		}
+
+		fmt.Println(post)
+
+		one, err := dbHandler.Posts.InsertOne(context.TODO(), post)
+		if err != nil {
+			return
+		}
+
+		json.NewEncoder(w).Encode(one)
+	} else {
+		w.Write([]byte("Method Not Allowed: " + r.Method))
+	}
+}
+
+func GetAllPostsOfUser(w http.ResponseWriter, r *http.Request, dbHandler BaseHandler) {
+	if r.Method == "GET" {
+		w.Header().Add("Content-Type", "application/json")
+		userId, _ := primitive.ObjectIDFromHex(strings.Split(r.URL.String(), "/posts/users/")[1])
+
+		//Sort posts in descending order by timestamp {the newest posts first}
+		opts := options.Find().SetSort(bson.D{{"posted_timestamp", -1}})
+
+		cursor, err := dbHandler.Posts.Find(context.TODO(), bson.M{"user_id": userId}, opts)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		var currentUserPosts []bson.M
+		if err = cursor.All(context.TODO(), &currentUserPosts); err != nil {
+			log.Fatal(err)
+		}
+		sendEncodedJsonErr := json.NewEncoder(w).Encode(currentUserPosts)
+		if sendEncodedJsonErr != nil {
+			return
+		}
+
+	} else {
+		w.Write([]byte("Method Not Allowed: " + r.Method))
+	}
+}
+
+func GetPost(w http.ResponseWriter, r *http.Request, dbHandler BaseHandler) {
+	if r.Method == "GET" {
+		w.Header().Add("Content-Type", "application/json")
+
+		var splitPostId = strings.Split(r.URL.String(), "/posts/")
+		if splitPostId[1] != "" {
+			postId, _ := primitive.ObjectIDFromHex(splitPostId[1])
+
+			var result bson.M
+			err := dbHandler.Posts.FindOne(context.TODO(), bson.M{"_id": postId}).Decode(&result)
+			if err != nil {
+				if err == mongo.ErrNoDocuments {
+					return
+				}
+				log.Fatal(err)
+			}
+			json.NewEncoder(w).Encode(result)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Invalid Endpoint"))
+		}
+
+	} else {
+		w.Write([]byte("Method Not Allowed: " + r.Method))
+	}
+}
---
+++ api/users.go
@@ -0,0 +1,103 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+import . "AppointyTask/models"
+
+func GetUser(w http.ResponseWriter, r *http.Request, dbHandler BaseHandler) {
+	if r.Method == "GET" {
+		w.Header().Add("Content-Type", "application/json")
+
+		var splitUserId = strings.Split(r.URL.String(), "/users/")
+		if splitUserId[1] != "" {
+			userId, _ := primitive.ObjectIDFromHex(splitUserId[1])
+
+			var result bson.M
+			err := dbHandler.Users.FindOne(context.TODO(), bson.M{"_id": userId}).Decode(&result)
+			if err != nil {
+				if err == mongo.ErrNoDocuments {
+					return
+				}
+				log.Fatal(err)
+			}
+			json.NewEncoder(w).Encode(result)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Invalid Endpoint"))
+		}
+
+	} else {
+		w.Write([]byte("Method Not Allowed: " + r.Method))
+	}
+}
+
+func AddUser(w http.ResponseWriter, r *http.Request, dbHandler BaseHandler) {
+	if r.Method == "POST" {
+
+		var target map[string]interface{}
+		body, _ := ioutil.ReadAll(r.Body)
+		json.Unmarshal(body, &target)
+
+		var user User
+
+		name, ok := target["name"]
+
+		if ok {
+			nameString, _ := name.(string)
+			user.Name = nameString
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Name Not Provided"))
+			return
+		}
+
+		email, ok := target["email"]
+
+		if ok {
+			emailString, _ := email.(string)
+			user.Email = emailString
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Email Not Provided"))
+			return
+		}
+
+		password, ok := target["password"]
+
+		if ok {
+			passwordString, _ := password.(string)
+			hashedPassword, passwordHashingErr := bcrypt.GenerateFromPassword([]byte(passwordString), 8)
+
+			if passwordHashingErr != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			user.Password = string(hashedPassword)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Password Not Provided"))
+			return
+		}
+
+		one, err := dbHandler.Users.InsertOne(context.TODO(), user)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		json.NewEncoder(w).Encode(one)
+	} else {
+		w.Write([]byte("Method Not Allowed: " + r.Method))
+	}
+}
