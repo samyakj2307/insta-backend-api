04dfbf6a8627466baf6b15736959c8536f5b3401
Samyak Jain <jainsamyak2300@gmail.com>
2021-10-09 13:18:51 +0530

    get post with post id endpoint added

--- main.go
+++ main.go
@@ -2,32 +2,22 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
-	"strings"
 	"time"
 )
 
 import . "AppointyTask/models"
+import . "AppointyTask/api"
 
 var dbHandler = BaseHandler{}
-
-//var allUsers = Users{}
-
-//var allPosts = Posts{}
-
-var ctx context.Context
 
 func makeDBConnection() *mongo.Client {
 
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
@@ -44,68 +34,38 @@ func main() {
 		if err != nil {
 			log.Println("Error Disconnecting Database")
 		}
-	}(client, ctx)
+	}(client, context.Background())
 
 	dbHandler.Db = client.Database("appointyInstaDB")
 
 	dbHandler.Users = dbHandler.Db.Collection("users")
 	dbHandler.Posts = dbHandler.Db.Collection("posts")
+	http.HandleFunc("/users", addUserEndpoint)
+	http.HandleFunc("/users/", getUserEndpoint)
 
-	http.HandleFunc("/users", addUser)
-	http.HandleFunc("/users/", getUser)
+	http.HandleFunc("/posts", addPostEndpoint)
+	http.HandleFunc("/posts/", getPostEndpoint)
+	http.HandleFunc("/posts/users/", getAllPostsOfUserEndpoint)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func getUser(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		w.Header().Add("Content-Type", "application/json")
-		userId, _ := primitive.ObjectIDFromHex(strings.Split(r.URL.String(), "/users/")[1])
-
-		cursor, err := dbHandler.Users.Find(ctx, bson.M{"_id": userId})
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		var currentUser []bson.M
-		if err = cursor.All(ctx, &currentUser); err != nil {
-			log.Fatal(err)
-		}
-
-		json.NewEncoder(w).Encode(currentUser[0])
-
-	} else {
-		w.Write([]byte("Method Not Allowed: " + r.Method))
-	}
+func addUserEndpoint(w http.ResponseWriter, r *http.Request) {
+	AddUser(w, r, dbHandler)
 }
 
-func addUser(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		decoder := json.NewDecoder(r.Body)
+func getUserEndpoint(w http.ResponseWriter, r *http.Request) {
+	GetUser(w, r, dbHandler)
+}
 
-		var user User
+func addPostEndpoint(w http.ResponseWriter, r *http.Request) {
+	AddPost(w, r, dbHandler)
+}
 
-		err := decoder.Decode(&user)
-		if err != nil {
-			panic(err)
-		}
+func getPostEndpoint(w http.ResponseWriter, r *http.Request) {
+	GetPost(w, r, dbHandler)
+}
 
-		hashedPassword, passwordHashingErr := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
-
-		if passwordHashingErr != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		user.Password = string(hashedPassword)
-
-		one, err := dbHandler.Users.InsertOne(ctx, user)
-		if err != nil {
-			return
-		}
-
-		json.NewEncoder(w).Encode(one)
-	} else {
-		w.Write([]byte("Method Not Allowed: " + r.Method))
-	}
+func getAllPostsOfUserEndpoint(w http.ResponseWriter, r *http.Request) {
+	GetAllPostsOfUser(w, r, dbHandler)
 }
